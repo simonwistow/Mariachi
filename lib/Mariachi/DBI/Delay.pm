4ee96cfcfe69b343951ac5cc0643cd346b892903
Richard Clamp <richardc@unixbeard.net>
2003-11-27 16:28:20 +0000

    more tidying

--- lib/Mariachi/DBI/Delay.pm
+++
@@ -1,9 +0,0 @@
-use strict;
-package Mariachi::DBI::Delay;
-use base 'Mariachi::DBI';
-use Class::Delay
-  methods => [qw( slacker_table set_up_table has_a has_many )],
-  release => [qw( set_db )],
-  reorder => sub { sort { $b->is_trigger <=> $a->is_trigger } @_ };
-
-1;
